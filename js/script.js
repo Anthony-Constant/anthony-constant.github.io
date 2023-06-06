aeddf3c0a5192fe48ad90d1da7378531f86b6f73
Anthony Constant <83375598+Anthony-Constant@users.noreply.github.com>
2023-06-06 18:03:09 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 var typed = new Typed('#typed', {
     strings: ["Hi there, I'm Anthony Constant"],
-    typeSpeed: 50,
+    typeSpeed: 70,
     cursorChar: '_'
     });
 
 var typed = new Typed('#subtyped', {
-    strings: [ 
-    "First Class Honours Bachelor of Science (BSc) in Computer Science"],
-    typeSpeed: 50,
+    strings: ["First Class Honours Bachelor of Science (BSc) Graduate in Computer Science"],
+    typeSpeed: 70,
     cursorChar: '_',
-    startDelay: 2000,
+    startDelay: 3000,
     backDelay: 2000
+    
         
      });
