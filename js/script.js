1c7fe5dc2c72f5559b3125aece5bdb237269cafa
Anthony Constant <83375598+Anthony-Constant@users.noreply.github.com>
2023-12-14 18:50:44 +0000

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-VANTA.NET({
+if (window.VANTA) window.VANTA.DOTS({
     el: "html",
     mouseControls: true,
     touchControls: true,
@@ -7,6 +7,13 @@ VANTA.NET({
     minWidth: 200.00,
     scale: 1.00,
     scaleMobile: 1.00,
+    showLines: false,
     color: 0x3fddff,
+    color2: 0xff8820,
     backgroundColor: 0x101820
-});
+  })
+  
+  _strk.push(function() {
+    setVanta()
+    window.edit_page.Event.subscribe( "Page.beforeNewOneFadeIn", setVanta )
+  })
